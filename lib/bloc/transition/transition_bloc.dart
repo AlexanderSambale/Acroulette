c51ad45b2aeedcb288286cebf3333f181ae76702
AlexanderSGNU <alexandersgnu@gmx.de>
2022-07-21 17:55:52 +0200

    Initial Commit with blocs

---
+++ lib/bloc/transition/transition_bloc.dart
@@ -0,0 +1,51 @@
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+import 'package:equatable/equatable.dart';
+import 'dart:math';
+
+part 'transition_event.dart';
+part 'transition_state.dart';
+
+const figures = ["bird", "star", "bat", "triangle", "backbird", "reversebird"];
+
+class TransitionBloc extends Bloc<TransitionEvent, TransitionState> {
+  TransitionBloc() : super(const TransitionState.initial()) {
+    on<NewTransitionEvent>((event, emit) {
+      state.figures.add(getRandomFigure());
+      emit(TransitionState(
+          state.figures, state.index + 1, TransitionStatus.create));
+    });
+    on<NextTransitionEvent>((event, emit) {
+      if (state.index + 1 < state.figures.length) {
+        emit(TransitionState(
+            state.figures, state.index + 1, TransitionStatus.next));
+      } else {
+        /// no next figure is available
+        emit(TransitionState(
+            state.figures, state.index, TransitionStatus.nomove));
+      }
+    });
+    on<PreviousTransitionEvent>((event, emit) {
+      if (state.index > 0) {
+        emit(TransitionState(
+            state.figures, state.index - 1, TransitionStatus.previous));
+      } else {
+        /// no previous figure is available
+        emit(TransitionState(
+            state.figures, state.index, TransitionStatus.nomove));
+      }
+    });
+    on<CurrentTransitionEvent>((event, emit) {
+      // just call out the name of the current figure
+      emit(TransitionState(
+          state.figures, state.index, TransitionStatus.current));
+    });
+  }
+
+  final rng = Random();
+
+  String getRandomFigure() {
+    final newFigureIndex = rng.nextInt(figures.length);
+    return figures[newFigureIndex];
+  }
+}
