f96f209390bcb4247ac1bd3cf2c781cafb7fe450
AlexanderSGNU <alexandersgnu@gmx.de>
2022-08-08 22:35:49 +0200

    Add form for customization of the commands

--- lib/widgets/settings.dart
+++ lib/widgets/settings.dart
@@ -8,8 +8,77 @@ class Settings extends StatefulWidget {
 }
 
 class _SettingsState extends State<Settings> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
-    return Text("Settings");
+    return Form(
+      key: _formKey,
+      child: ListView(
+        children: <Widget>[
+          Text('next position'),
+          TextFormField(
+            decoration: const InputDecoration(
+              hintText: 'next position',
+            ),
+            validator: (String? value) {
+              if (value == null || value.isEmpty) {
+                return 'Please enter some text';
+              }
+              return null;
+            },
+          ),
+          Text('new position'),
+          TextFormField(
+            decoration: const InputDecoration(
+              hintText: 'new position',
+            ),
+            validator: (String? value) {
+              if (value == null || value.isEmpty) {
+                return 'Please enter some text';
+              }
+              return null;
+            },
+          ),
+          Text('previous position'),
+          TextFormField(
+            decoration: const InputDecoration(
+              hintText: 'previous position',
+            ),
+            validator: (String? value) {
+              if (value == null || value.isEmpty) {
+                return 'Please enter some text';
+              }
+              return null;
+            },
+          ),
+          Text('current position'),
+          TextFormField(
+            decoration: const InputDecoration(
+              hintText: 'current position',
+            ),
+            validator: (String? value) {
+              if (value == null || value.isEmpty) {
+                return 'Please enter some text';
+              }
+              return null;
+            },
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
+            child: ElevatedButton(
+              onPressed: () {
+                // Validate will return true if the form is valid, or false if
+                // the form is invalid.
+                if (_formKey.currentState!.validate()) {
+                  // Process data.
+                }
+              },
+              child: const Text('Submit'),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
