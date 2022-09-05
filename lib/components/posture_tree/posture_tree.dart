51d2e0942f898628781ac7c28297a986ba97e6df
AlexanderSGNU <alexandersgnu@gmx.de>
2022-09-05 22:50:36 +0200

    Refactor PostureTree and story for it

--- lib/components/posture_tree/posture_tree.dart
+++ lib/components/posture_tree/posture_tree.dart
@@ -1,28 +1,41 @@
 import 'dart:collection';
 
+import 'package:acroulette/components/posture_tree/posture_category_item.dart';
+import 'package:acroulette/components/posture_tree/posture_list_item.dart';
+import 'package:acroulette/models/acro_node.dart';
+import 'package:acroulette/models/node.dart';
 import 'package:flutter/material.dart';
-
-
 
 class PostureTree extends StatelessWidget {
   const PostureTree({super.key, required this.tree});
 
-  final LinkedHashSet< tree;
+  final Node<AcroNode> tree;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        height: 50,
-        child: Row(
-          children: [
-            Center(child: Text(postureLabel)),
-            Switch(value: isSwitched, onChanged: (value) {}),
-            IconButton(
-              icon: const Icon(Icons.delete),
-              tooltip: 'Delete position',
-              onPressed: () {},
-            )
-          ],
-        ));
+    if (tree.isLeaf) {
+      if (tree.value == null) {
+        return Container();
+      } else {
+        return PostureListItem(
+            isSwitched: tree.value!.isSwitched,
+            postureLabel: tree.value!.label,
+            onChanged: tree.value!.onSwitchChange,
+            delete: tree.value!.delete);
+      }
+    } else {
+      return ListView.builder(
+          itemCount: tree.children.length + 1,
+          itemBuilder: (context, index) {
+            if (index == 0) {
+              return PostureCategoryItem(
+                  categoryLabel: tree.value!.label,
+                  isSwitched: tree.value!.isSwitched,
+                  isExpanded: true);
+            } else {
+              return PostureTree(tree: tree.children.elementAt(index - 1));
+            }
+          });
+    }
   }
 }
--- widgetbook/widgetbook.dart
+++ widgetbook/widgetbook.dart
@@ -1,7 +1,31 @@
+import 'dart:collection';
+
 import 'package:acroulette/components/posture_tree/posture_category_item.dart';
 import 'package:acroulette/components/posture_tree/posture_list_item.dart';
+import 'package:acroulette/components/posture_tree/posture_tree.dart';
+import 'package:acroulette/models/acro_node.dart';
+import 'package:acroulette/models/node.dart';
 import 'package:flutter/material.dart';
 import 'package:widgetbook/widgetbook.dart';
+
+var onSwitch = (bool isOn) => {};
+var delete = () => {};
+
+createSimpleTree() {
+  LinkedHashSet<Node<AcroNode>> children = LinkedHashSet<Node<AcroNode>>(
+      equals: (n0, n1) => n0 == n1, hashCode: (n2) => n2.hashCode);
+
+  children
+      .add(Node.createLeaf(AcroNode('leaf1', true, 'leaf1', onSwitch, delete)));
+  children.add(
+      Node.createLeaf(AcroNode('leaf2', false, 'leaf2', onSwitch, delete)));
+  children
+      .add(Node.createLeaf(AcroNode('leaf3', true, 'leaf3', onSwitch, delete)));
+
+  Node<AcroNode> simpleTree =
+      Node(children, false, AcroNode('root', true, 'root', onSwitch, delete));
+  return simpleTree;
+}
 
 class HotReload extends StatelessWidget {
   const HotReload({Key? key}) : super(key: key);
@@ -63,6 +87,8 @@ class HotReload extends StatelessWidget {
                                 label: 'Label',
                                 initialValue: 'Posture1',
                               ),
+                              onChanged: onSwitch,
+                              delete: delete,
                             )),
                   ],
                 ),
@@ -92,33 +118,8 @@ class HotReload extends StatelessWidget {
                   useCases: [
                     WidgetbookUseCase(
                         name: 'Default',
-                        builder: (context) => PostureListItem(
-                              isSwitched: context.knobs.boolean(
-                                label: 'isSwitched',
-                                initialValue: false,
-                              ),
-                              postureLabel: context.knobs.text(
-                                label: 'Label',
-                                initialValue: 'Category',
-                              ),
-                            )),
-                  ],
-                ),
-                WidgetbookComponent(
-                  name: 'PostureList',
-                  useCases: [
-                    WidgetbookUseCase(
-                        name: 'Default',
-                        builder: (context) => PostureListItem(
-                              isSwitched: context.knobs.boolean(
-                                label: 'isSwitched',
-                                initialValue: false,
-                              ),
-                              postureLabel: context.knobs.text(
-                                label: 'Label',
-                                initialValue: 'Category',
-                              ),
-                            )),
+                        builder: (context) =>
+                            PostureTree(tree: createSimpleTree()))
                   ],
                 ),
               ],
