b2057042ebcecb1b795871b03e75d155bf0b0a6c
AlexanderSGNU <alexandersgnu@gmx.de>
2022-10-22 00:07:22 +0200

    Remove add form for posture tab

--- lib/widgets/positions.dart
+++ lib/widgets/positions.dart
@@ -1,7 +1,6 @@
 import 'package:acroulette/bloc/position_administration/position_administration_bloc.dart';
 import 'package:acroulette/components/posture_tree/posture_tree.dart';
 import 'package:acroulette/main.dart';
-import 'package:acroulette/models/position.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -21,35 +20,6 @@ class _PositionsState extends State<Positions> {
         key: _formKey,
         child: ListView(
           children: <Widget>[
-            const Text("Positions"),
-            const Text("New Position"),
-            TextFormField(
-              decoration: const InputDecoration(
-                hintText: "Add position here",
-              ),
-              validator: (String? value) {
-                if (value == null || value.isEmpty) {
-                  return 'Please enter some text';
-                }
-                if (objectbox.getPosition(value) != null) {
-                  return 'Position $value already exists!';
-                }
-                return null;
-              },
-              onSaved: (newValue) =>
-                  objectbox.positionBox.put(Position(newValue!)),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 16.0),
-              child: ElevatedButton(
-                onPressed: () {
-                  if (_formKey.currentState!.validate()) {
-                    _formKey.currentState!.save();
-                  }
-                },
-                child: const Text('Submit'),
-              ),
-            ),
             BlocProvider(
               create: (_) => PositionAdministrationBloc(objectbox),
               child: BlocBuilder<PositionAdministrationBloc,
@@ -63,6 +33,7 @@ class _PositionsState extends State<Positions> {
                       tree: state.tree,
                       onSwitched: positionAdministrationBloc.onSwitch,
                       toggleExpand: positionAdministrationBloc.toggleExpand,
+                      path: const [],
                     );
                   }),
             )
