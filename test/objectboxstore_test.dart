6c85b3d68357a83d2bf0281bb8e5adecc6427b6a
AlexanderSGNU <alexandersgnu@gmx.de>
2022-10-23 21:20:05 +0200

    Add objectboxstore test for findParent

---
+++ test/objectboxstore_test.dart
@@ -0,0 +1,31 @@
+import 'dart:io';
+
+import 'package:acroulette/constants/nodes.dart';
+import 'package:acroulette/database/objectbox.g.dart';
+import 'package:acroulette/models/node.dart';
+import 'package:acroulette/objectboxstore.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  late Store store;
+  late ObjectBox objectbox;
+  final dir = Directory('testdata');
+
+  setUp(() async {
+    if (dir.existsSync()) dir.deleteSync(recursive: true);
+    await dir.create();
+    store = await openStore(directory: dir.path);
+    objectbox = await ObjectBox.create(store);
+  });
+
+  tearDown(() {
+    store.close();
+    if (dir.existsSync()) dir.deleteSync(recursive: true);
+  });
+
+  test('findParent', () {
+    Node parent = objectbox.findParent(objectbox.nodeBox.getAll().last);
+    expect(parent.isExpanded, true);
+    expect(parent.value.target!.label, basicPostures);
+  });
+}
