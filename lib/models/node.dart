9c20448fa6a97fa9b19da156b6e3555b3615ef68
AlexanderSGNU <alexandersgnu@gmx.de>
2022-10-24 18:02:07 +0200

    Refactor node to have flag is leaf Remove dependency between isLeaf and children

--- lib/models/node.dart
+++ lib/models/node.dart
@@ -5,33 +5,37 @@ import 'package:objectbox/objectbox.dart';
 class Node {
   int id = 0;
 
-  bool get isLeaf => children.isEmpty;
+  bool isLeaf;
   ToMany<Node> children = ToMany<Node>();
   ToOne<AcroNode> value = ToOne<AcroNode>();
   bool isExpanded;
   String? get label => value.target?.label;
 
-  Node(this.children, this.value, {this.isExpanded = true});
+  Node(this.children, this.value,
+      {this.isLeaf = false, this.isExpanded = true});
 
   Node.createCategory(List<Node> children, AcroNode acroNode,
-      {this.isExpanded = true}) {
+      {this.isLeaf = false, this.isExpanded = true}) {
     this.children.addAll(children);
     value.target = acroNode;
   }
 
-  Node.createLeaf(acroNode, {this.isExpanded = true}) {
+  Node.createLeaf(acroNode, {this.isLeaf = true, this.isExpanded = true}) {
     value.target = acroNode;
   }
 
   addNode(Node node) {
+    if (isLeaf) Exception('This is a leaf! Nodes cannot be added.');
     children.add(node);
   }
 
   addAll(List<Node> nodes) {
+    if (isLeaf) Exception('This is a leaf! Nodes cannot be added.');
     children.addAll(nodes);
   }
 
   removeNode(Node node) {
+    if (isLeaf) Exception('This is a leaf! Nodes cannot be removed.');
     children.remove(node);
   }
 }
