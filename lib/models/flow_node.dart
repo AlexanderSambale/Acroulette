491b0e8a26c91e7b2e884d7590f4c03ce1af9199
AlexanderSGNU <alexandersgnu@gmx.de>
2023-05-12 09:47:52 +0200

    Add conversion to JSON and back for FlowNode

--- lib/models/flow_node.dart
+++ lib/models/flow_node.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
+
 import 'package:objectbox/objectbox.dart';
+
+const String cName = "name";
+const String cPositions = "positions";
 
 @Entity()
 class FlowNode {
@@ -9,4 +14,18 @@ class FlowNode {
   bool isExpanded;
 
   FlowNode(this.name, this.positions, {this.isExpanded = true});
+
+  @override
+  String toString() {
+    return '''{"$cName":"$name", "$cPositions":${jsonEncode(positions)}}''';
+  }
+
+  static FlowNode createFromString(String source) {
+    Map decoded = jsonDecode(source);
+    List<String> flowPositions = [];
+    for (var position in decoded[cPositions]) {
+      flowPositions.add(position);
+    }
+    return FlowNode(decoded[cName], flowPositions);
+  }
 }
