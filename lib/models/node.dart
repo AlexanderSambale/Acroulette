e8b6c3c16706e8f87f5c45fdba1ae1e621306e30
AlexanderSGNU <alexandersgnu@gmx.de>
2023-05-13 10:59:31 +0200

    Add convert functions, == and hashCode for Node

--- lib/models/node.dart
+++ lib/models/node.dart
@@ -1,5 +1,12 @@
+import 'dart:convert';
+
 import 'package:acroulette/models/acro_node.dart';
 import 'package:objectbox/objectbox.dart';
+
+const String isLeafKey = "isLeaf";
+const String isExpandedKey = "isExpanded";
+const String childrenKey = "children";
+const String valueKey = "value";
 
 @Entity()
 class Node {
@@ -38,4 +45,53 @@ class Node {
     if (isLeaf) Exception('This is a leaf! Nodes cannot be removed.');
     children.remove(node);
   }
+
+  @override
+  String toString() {
+    String result = '''{''';
+    if (children.isNotEmpty) {
+      List<String> childrenAsJson = [];
+      for (var child in children) {
+        childrenAsJson.add(child.toString());
+      }
+      result = '''$result"$childrenKey": $childrenAsJson,''';
+    }
+    result = '''$result
+  "$isLeafKey": $isLeaf,
+  "$isExpandedKey": $isExpanded,
+  "$valueKey": ${value.target!}
+}''';
+    return result;
+  }
+
+  static Node createFromString(String source) {
+    Map decoded = jsonDecode(source);
+    return createFromMap(decoded);
+  }
+
+  static Node createFromMap(Map decoded) {
+    List<Node> children = [];
+    if (decoded[childrenKey] != null) {
+      for (var child in decoded[childrenKey]) {
+        children.add(Node.createFromMap(child));
+      }
+    }
+    AcroNode value = AcroNode.createFromMap(decoded[valueKey]);
+    return Node.createCategory(children, value,
+        isLeaf: decoded[isLeafKey], isExpanded: decoded[isExpandedKey]);
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (other is! Node) return false;
+    if (other.isExpanded != isExpanded) return false;
+    if (other.isLeaf != isLeaf) return false;
+    if (other.value.target != value.target) return false;
+    if (other.children != other.children) return false;
+    return true;
+  }
+
+  @override
+  int get hashCode => Object.hash(
+      isLeaf.hashCode, isExpanded.hashCode, children.hashCode, value.hashCode);
 }
