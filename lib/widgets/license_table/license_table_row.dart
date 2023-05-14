938baabd7c646bf7c3f64cd7f080746ee498b371
AlexanderSGNU <alexandersgnu@gmx.de>
2023-05-14 18:45:47 +0200

    Update LicenseTableRow with clickable Text Use RichText and TextSpan instead of Button

--- lib/widgets/license_table/license_table_row.dart
+++ lib/widgets/license_table/license_table_row.dart
@@ -1,5 +1,6 @@
 import 'package:acroulette/oss_licenses.dart';
 import 'package:acroulette/widgets/license_table/show_license_dialog.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 
@@ -8,21 +9,32 @@ TableRow createLicenseTableRow(Package package, BuildContext context) {
     children: <Widget>[
       Text(package.name),
       package.license == null
-          ? Text("")
-          : ElevatedButton(
-              onPressed: () => showDialog(
-                      context: context,
-                      builder: (BuildContext context) {
-                        return ShowLicenseDialog(license: package.license!);
-                      }).then((exit) {
-                    if (exit) return;
-                  }),
-              child: const Text('Show License')),
+          ? Container()
+          : RichText(
+              text: TextSpan(
+                  text: 'Show License',
+                  recognizer: TapGestureRecognizer()
+                    ..onTap = () => showDialog(
+                            context: context,
+                            builder: (BuildContext context) {
+                              return ShowLicenseDialog(
+                                  license: package.license!);
+                            }).then((exit) {
+                          if (exit) return;
+                        }),
+                  style: const TextStyle(
+                    color: Colors.blue,
+                  ))),
       package.homepage == null
-          ? Text("")
-          : ElevatedButton(
-              onPressed: () => {launchUrl(Uri.parse(package.homepage!))},
-              child: Text(package.homepage!))
+          ? Container()
+          : RichText(
+              text: TextSpan(
+                  text: package.homepage!,
+                  recognizer: TapGestureRecognizer()
+                    ..onTap = () => {launchUrl(Uri.parse(package.homepage!))},
+                  style: const TextStyle(
+                    color: Colors.blue,
+                  )))
     ],
   );
 }
