e5ac5be1569a50bc261dcba509fa983ac856ed47
AlexanderSGNU <alexandersgnu@gmx.de>
2023-05-14 11:31:40 +0200

    Start implementing import

--- lib/models/helper/import_export/import.dart
+++ lib/models/helper/import_export/import.dart
@@ -1,16 +1,33 @@
+import 'dart:convert';
+import 'dart:io';
 import 'dart:typed_data';
 
+import 'package:acroulette/constants/import_export.dart';
+import 'package:acroulette/models/flow_node.dart';
+import 'package:acroulette/models/node.dart';
+import 'package:acroulette/objectboxstore.dart';
 import 'package:pick_or_save/pick_or_save.dart';
 
-void import() async {
+void import(ObjectBox objectBox) async {
   List<String>? result = await PickOrSave().filePicker(
-    params: FilePickerParams(getCachedFilePath: false),
+    params: FilePickerParams(),
   );
   String filePath = result![0];
-  // data = readFile(filePath);
-  // importData(data);
+  File file = File(filePath);
+  String content = await file.readAsString();
+  importData(content, objectBox);
 }
 
 String convertUint8ListToString(Uint8List uint8list) {
   return String.fromCharCodes(uint8list);
+}
+
+void importData(String data, ObjectBox objectBox) {
+  Map decoded = jsonDecode(data);
+  List<FlowNode> flows = [];
+  for (Map flow in decoded[flowsKey]) {
+    flows.add(FlowNode.createFromMap(flow));
+  }
+  objectBox.nodeBox.put(Node.createFromMap(decoded[nodesKey]));
+  objectBox.flowNodeBox.putMany(flows);
 }
