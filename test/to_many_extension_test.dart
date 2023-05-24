f7242e8163b1fb321be35d6fb849f0d97419bbf0
AlexanderSGNU <alexandersgnu@gmx.de>
2023-05-24 16:17:01 +0200

    Update to many extension test

--- test/to_many_extension_test.dart
+++ test/to_many_extension_test.dart
@@ -2,7 +2,7 @@ import 'package:acroulette/database/objectbox.g.dart';
 import 'package:acroulette/models/acro_node.dart';
 import 'package:acroulette/models/helper/objectbox/to_many_extension.dart';
 import 'package:acroulette/models/node.dart';
-import 'package:test/test.dart';
+import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   late ToMany<Node> children;
