c3423b23cf8884c57e1d43a2eb40bab7d05b7bb8
Alexander Sambale <Alexander.Sambale@samxela.eu>
2024-03-27 17:53:12 +0100

    Update to many extension for isar

--- lib/models/helper/objectbox/to_many_extension.dart
+++ lib/models/helper/objectbox/to_many_extension.dart
@@ -1,25 +1,15 @@
 import 'package:acroulette/models/node.dart';
+import 'package:isar/isar.dart';
 
-extension ToManyExtension on ToMany<Node> {
+extension ToManyExtension on IsarLinks<Node> {
   bool containsElementWithId(int id) {
-    int length = this.length;
-    for (int i = 0; i < length; i++) {
-      if (this[i].id == id) return true;
-      if (length != this.length) {
-        throw ConcurrentModificationError(this);
-      }
-    }
-    return false;
+    var links = where((element) => element.id == id);
+    return links.isNotEmpty;
   }
 
   bool containsElementWithLabel(bool isPosture, String label) {
-    int length = this.length;
-    for (int i = 0; i < length; i++) {
-      if (this[i].isLeaf == isPosture && this[i].label! == label) return true;
-      if (length != this.length) {
-        throw ConcurrentModificationError(this);
-      }
-    }
-    return false;
+    var links = where(
+        (element) => element.isLeaf == isPosture && element.label == label);
+    return links.isNotEmpty;
   }
 }
