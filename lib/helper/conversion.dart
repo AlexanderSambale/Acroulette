1769bd0e640cb34cc514c94bd12ec94d6d930e24
Alexander Sambale <Alexander.Sambale@samxela.eu>
2024-07-30 10:52:41 +0200

    Implement conversion functions Implement stringListToUint8List Implement uint8ListToStringList

--- lib/helper/conversion.dart
+++ lib/helper/conversion.dart
@@ -4,11 +4,42 @@ import 'dart:typed_data';
 String joinString = '\u{1D434}\u{26A1}\u{1D4FD}';
 
 Uint8List stringListToUint8List(List<String> input) {
-  Uint8List result = Uint8List(8);
+  List<int> lowIntegerBytes = [];
+  Uint16List uint16List = Uint16List.fromList(input.join(joinString).codeUnits);
+  for (var u16integer in uint16List) {
+    lowIntegerBytes.add(getIntHighByte(u16integer));
+    lowIntegerBytes.add(getIntLowByte(u16integer));
+  }
+  Uint8List result = Uint8List.fromList(lowIntegerBytes);
   return result;
 }
 
 List<String> uint8ListToStringList(Uint8List uint8List) {
-  List<String> resultList = [];
+  List<int> lowIntegerBytes = uint8List.toList();
+  List<int> charCodes = [];
+  int lowByte = 0;
+  int highByte = 0;
+  int u16integer = 0;
+  bool odd = true;
+  for (int integer in lowIntegerBytes) {
+    if (odd) {
+      highByte = integer << 8;
+      odd = false;
+    } else {
+      lowByte = integer;
+      u16integer = highByte | lowByte;
+      charCodes.add(u16integer);
+      odd = true;
+    }
+  }
+  List<String> resultList = String.fromCharCodes(charCodes).split(joinString);
   return resultList;
+}
+
+int getIntHighByte(int input) {
+  return (input >> 8) & 0xFF;
+}
+
+int getIntLowByte(int input) {
+  return input & 0xFF;
 }
