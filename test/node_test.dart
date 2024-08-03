928c4525be120304df407188407835423eb171d5
Alexander Sambale <Alexander.Sambale@samxela.eu>
2024-08-03 19:04:00 +0200

    Correct node_test

--- test/node_test.dart
+++ test/node_test.dart
@@ -1,43 +1,47 @@
-import 'package:acroulette/models/entities/acro_node.dart';
 import 'package:acroulette/models/node.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   test('LeafNode', () {
-    Node leaf = Node.createLeaf(AcroNode(false, 'test'));
+    Node leaf = Node.createLeaf(label: 'test');
     expect(leaf.children.isEmpty, equals(true));
     expect(leaf.isLeaf, equals(true));
   });
 
   test('Category with some posture items', () {
     const String leafLabel = 'leaf';
-    Node leaf1 = Node.createLeaf(AcroNode(false, "${leafLabel}1"));
-    Node leaf2 = Node.createLeaf(AcroNode(false, "${leafLabel}2"));
-    Node leaf3 = Node.createLeaf(AcroNode(false, "${leafLabel}3"));
-    AcroNode rootAcroNode = AcroNode(true, "root");
-    Node category = Node.createCategory([leaf1, leaf2, leaf3], rootAcroNode);
+    Node leaf1 = Node.createLeaf(label: "${leafLabel}1");
+    Node leaf2 = Node.createLeaf(label: "${leafLabel}2");
+    Node leaf3 = Node.createLeaf(label: "${leafLabel}3");
+    String label = "root";
+    Node category = Node.optional(
+        children: [leaf1, leaf2, leaf3], isLeaf: false, label: label);
     category.isExpanded = false;
     expect(category.children.isEmpty, equals(false));
     expect(category.children.length, equals(3));
     expect(category.isLeaf, equals(false));
-    expect(category.acroNode, equals(rootAcroNode));
+    expect(category.label, equals(label));
     expect(category.children.contains(leaf3), equals(true));
   });
 
   test('convert Node, which is a category, to String and back', () {
     const String leafLabel = 'leaf';
-    AcroNode rootAcroNode = AcroNode(true, "root");
-    Node leaf1 = Node.createLeaf(AcroNode(false, "${leafLabel}1"));
-    Node leaf2 = Node.createLeaf(AcroNode(false, "${leafLabel}2"));
-    Node leaf3 = Node.createLeaf(AcroNode(false, "${leafLabel}3"));
-    Node category = Node.createCategory([leaf1, leaf2, leaf3], rootAcroNode);
-    category.isExpanded = false;
+    Node leaf1 = Node.createLeaf(label: "${leafLabel}1");
+    Node leaf2 = Node.createLeaf(label: "${leafLabel}2");
+    Node leaf3 = Node.createLeaf(label: "${leafLabel}3");
+    String label = "root";
+    Node category = Node.optional(
+      children: [leaf1, leaf2, leaf3],
+      isLeaf: false,
+      label: label,
+      isExpanded: false,
+    );
     String categoryAsJsonString = category.toString();
     Node categoryTransformed = Node.createFromString(categoryAsJsonString)[0];
     expect(category.children.isEmpty, categoryTransformed.children.isEmpty);
     expect(category.children.length, categoryTransformed.children.length);
     expect(category.isLeaf, categoryTransformed.isLeaf);
-    expect(category.acroNode, categoryTransformed.acroNode);
+    expect(category.label, categoryTransformed.label);
     expect(
       category.children.elementAt(2),
       categoryTransformed.children.elementAt(2),
@@ -46,13 +50,12 @@ void main() {
 
   test('convert Node, which is a leaf, to String and back', () {
     const String leafLabel = 'leaf';
-    AcroNode rootAcroNode = AcroNode(false, leafLabel);
-    Node leaf = Node.createLeaf(rootAcroNode);
+    Node leaf = Node.createLeaf(label: leafLabel);
     String leafAsJsonString = leaf.toString();
     Node leafTransformed = Node.createFromString(leafAsJsonString)[0];
     expect(leafTransformed.children.isEmpty, leaf.children.isEmpty);
     expect(leafTransformed.isLeaf, leaf.isLeaf);
-    expect(leafTransformed.acroNode, leaf.acroNode);
+    expect(leafTransformed.label, leaf.label);
     expect(leafTransformed.isExpanded, leaf.isExpanded);
     expect(leafTransformed.label, leaf.label);
   });
