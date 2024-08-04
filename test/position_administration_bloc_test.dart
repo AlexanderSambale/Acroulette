1a487d70f58efbeb3d95830386da87a77a4d4ce7
Alexander Sambale <Alexander.Sambale@samxela.eu>
2024-08-04 13:23:58 +0000

    Update position administraion bloc test

--- test/position_administration_bloc_test.dart
+++ test/position_administration_bloc_test.dart
@@ -7,25 +7,35 @@ import 'package:acroulette/models/node.dart';
 import 'package:acroulette/db_controller.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-Node createSimpleTree(
-    {String rootName = 'root',
-    String leaf1Name = 'leaf1',
-    String leaf2Name = 'leaf2',
-    String leaf3Name = 'leaf3'}) {
-  Node leaf1 = Node.createLeaf(AcroNode(true, leaf1Name));
-  Node leaf2 = Node.createLeaf(AcroNode(false, leaf2Name));
-  Node leaf3 = Node.createLeaf(AcroNode(true, leaf3Name));
-  Node category =
-      Node.optional(children: [leaf1, leaf2, leaf3], AcroNode(true, rootName));
+Node createSimpleTree({
+  String rootName = 'root',
+  String leaf1Name = 'leaf1',
+  String leaf2Name = 'leaf2',
+  String leaf3Name = 'leaf3',
+}) {
+  Node leaf1 = Node.createLeaf(label: leaf1Name);
+  Node leaf2 = Node.createLeaf(
+    isSwitched: false,
+    isEnabled: false,
+    label: leaf2Name,
+  );
+  Node leaf3 = Node.createLeaf(label: leaf3Name);
+  Node category = Node.optional(
+    children: [leaf1, leaf2, leaf3],
+    label: rootName,
+  );
   return category;
 }
 
 Node createComplexTree() {
-  Node root = Node.optional(children: [
-    createSimpleTree(rootName: 'root1'),
-    createSimpleTree(rootName: 'root2'),
-    createSimpleTree(rootName: 'root3')
-  ], AcroNode(true, 'root'));
+  Node root = Node.optional(
+    children: [
+      createSimpleTree(rootName: 'root1'),
+      createSimpleTree(rootName: 'root2'),
+      createSimpleTree(rootName: 'root3'),
+    ],
+    label: 'root',
+  );
   return root;
 }
 
