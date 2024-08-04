7a0a69486ed0f80e6479cdbb5d7233075184a6d9
Alexander Sambale <Alexander.Sambale@samxela.eu>
2024-08-04 14:33:28 +0000

    Update db_controller_test

--- test/db_controller_test.dart
+++ test/db_controller_test.dart
@@ -1,36 +1,27 @@
-import 'dart:io';
-
 import 'package:acroulette/constants/nodes.dart';
+import 'package:acroulette/models/database.dart';
 import 'package:acroulette/models/node.dart';
 import 'package:acroulette/db_controller.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
-  late Isar store;
+
+  late AppDatabase database;
   late DBController dbController;
-  final dir = Directory('testdata');
 
   setUp(() async {
-    if (dir.existsSync()) dir.deleteSync(recursive: true);
-    await dir.create();
-    await Isar.initializeIsarCore(download: true);
-    store = await Isar.open(
-      [],
-      directory: dir.path,
-    );
-    dbController = await DBController.create(store);
+    database = await $FloorAppDatabase.inMemoryDatabaseBuilder().build();
+    dbController = await DBController.create(database);
   });
 
-  tearDown(() {
-    store.close();
-    if (dir.existsSync()) dir.deleteSync(recursive: true);
+  tearDown(() async {
+    await database.close();
   });
 
   test('findParent', () {
-    Node parent = dbController
-        .findParent(dbController.nodeBox.where().findAllSync().last)!;
+    Node parent = dbController.findParent(dbController.nodeBox.findAll());
     expect(parent.isExpanded, true);
-    expect(parent.label!, basicPostures);
+    expect(parent.label, basicPostures);
   });
 }
