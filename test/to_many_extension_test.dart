9f72ed55ab403c92df911628ead2bcc5f94c87b2
Alexander Sambale <Alexander.Sambale@samxela.eu>
2024-08-05 08:22:16 +0000

    Update to many extension test

--- test/to_many_extension_test.dart
+++ test/to_many_extension_test.dart
@@ -1,24 +1,21 @@
-import 'package:acroulette/models/entities/acro_node.dart';
+import 'package:acroulette/helper/objectbox/to_many_extension.dart';
 import 'package:acroulette/models/node.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  late IsarLinks<Node> children;
-
-  setUp(() async {
-    children = IsarLinks<Node>();
-  });
-
   test('containsElementWithLabel', () {
     String testLabel = 'test';
-    Node testPosture = Node.createLeaf(AcroNode(true, testLabel));
-    Node testCategory = Node.createCategory([], AcroNode(true, testLabel));
+    List<Node> children = [];
     expect(children.containsElementWithLabel(false, testLabel), false);
     expect(children.containsElementWithLabel(true, testLabel), false);
+
+    Node testPosture = Node.createLeaf(label: testLabel);
     children.add(testPosture);
     expect(children.containsElementWithLabel(true, testLabel), true);
     expect(children.containsElementWithLabel(false, testLabel), false);
     children.remove(testPosture);
+
+    Node testCategory = Node.optional(label: testLabel);
     children.add(testCategory);
     expect(children.containsElementWithLabel(false, testLabel), true);
     expect(children.containsElementWithLabel(true, testLabel), false);
