d290f78f47b4e4af83b84819599facbddcbd41f7
Alexander Sambale <Alexander.Sambale@samxela.eu>
2024-08-05 11:34:38 +0000

    Remove deleteById Use removeObject

--- lib/models/dao/node_without_parent_dao.dart
+++ lib/models/dao/node_without_parent_dao.dart
@@ -4,14 +4,11 @@ import 'package:floor/floor.dart';
 @dao
 abstract class NodeWithoutParentDao {
   @insert
-  Future<void> insertObject(NodeWithoutParent nodeNode);
+  Future<int> insertObject(NodeWithoutParent nodeNode);
 
   @delete
   Future<void> removeObject(NodeWithoutParent nodeNode);
 
   @Query('SELECT * FROM NodeWithoutParent')
   Future<List<NodeWithoutParent>> findAll();
-
-  @Query('DELETE * FROM NodeWithoutParent WHERE nodeId = :id')
-  Future<void> deleteById(int id);
 }
