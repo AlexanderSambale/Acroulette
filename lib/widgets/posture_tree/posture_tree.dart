57f8d601f5f8e863c3c7e963d7b75fabd1dbc0ef
Alexander Sambale <Alexander.Sambale@samxela.eu>
2024-08-25 15:48:20 +0200

    Use  ExpansionPanelList in PostureTree

--- lib/widgets/posture_tree/posture_tree.dart
+++ lib/widgets/posture_tree/posture_tree.dart
@@ -48,47 +48,44 @@ class PostureTree extends StatelessWidget {
               path: newPath,
               enabled: tree.isEnabled));
     }
-    return Card(
-        child: ListView.builder(
-            shrinkWrap: true,
-            itemCount: tree.children.length + 1,
-            itemBuilder: (context, index) {
-              if (index == 0) {
-                return PostureCategoryItem(
-                  categoryLabel: tree.label,
-                  isSwitched: tree.isSwitched,
-                  onChanged: (isOn) => onSwitched(isOn, tree),
-                  onEditClick: (String? value) =>
-                      onEditClick(tree, false, value),
-                  onDeleteClick: () => onDeleteClick(tree),
-                  onSaveClick: (bool isPosture, String? value) =>
-                      onSaveClick(tree, isPosture, value),
-                  toggleExpand: () => toggleExpand(tree),
-                  isExpanded: tree.isExpanded,
-                  enabled: tree.isEnabled,
-                  path: newPath,
-                  listAllNodesRecursively: () => listAllNodesRecursively(tree),
-                  validator: (bool isPosture, String? value) {
-                    if (validator == null) return null;
-                    return validator!(tree, isPosture, value);
-                  },
-                );
-              }
-              if (tree.isExpanded) {
-                return Container(
-                    margin: const EdgeInsets.only(left: 24),
-                    child: PostureTree(
-                        tree: tree.children.elementAt(index - 1),
-                        onSwitched: onSwitched,
-                        toggleExpand: toggleExpand,
-                        onEditClick: onEditClick,
-                        onDeleteClick: onDeleteClick,
-                        onSaveClick: onSaveClick,
-                        path: newPath,
-                        listAllNodesRecursively: listAllNodesRecursively,
-                        validator: validator));
-              }
-              return Container();
-            }));
+    return ExpansionPanelList(
+      children: tree.children.map((Node child) {
+        return ExpansionPanel(
+          headerBuilder: (BuildContext context, bool isExpanded) {
+            return PostureCategoryItem(
+              categoryLabel: tree.label,
+              isSwitched: tree.isSwitched,
+              onChanged: (isOn) => onSwitched(isOn, tree),
+              onEditClick: (String? value) => onEditClick(tree, false, value),
+              onDeleteClick: () => onDeleteClick(tree),
+              onSaveClick: (bool isPosture, String? value) =>
+                  onSaveClick(tree, isPosture, value),
+              toggleExpand: () => toggleExpand(tree),
+              isExpanded: tree.isExpanded,
+              enabled: tree.isEnabled,
+              path: newPath,
+              listAllNodesRecursively: () => listAllNodesRecursively(tree),
+              validator: (bool isPosture, String? value) {
+                if (validator == null) return null;
+                return validator!(tree, isPosture, value);
+              },
+            );
+          },
+          body: Container(
+            margin: const EdgeInsets.only(left: 24),
+            child: PostureTree(
+                tree: child,
+                onSwitched: onSwitched,
+                toggleExpand: toggleExpand,
+                onEditClick: onEditClick,
+                onDeleteClick: onDeleteClick,
+                onSaveClick: onSaveClick,
+                path: newPath,
+                listAllNodesRecursively: listAllNodesRecursively,
+                validator: validator),
+          ),
+        );
+      }).toList(),
+    );
   }
 }
