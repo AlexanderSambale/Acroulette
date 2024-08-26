8cdb9cda98a29207161324fb044b0837730ea5b6
Alexander Sambale <Alexander.Sambale@samxela.eu>
2024-08-26 10:23:23 +0200

    Make posture list item slidable

--- lib/widgets/posture_tree/posture_list_item.dart
+++ lib/widgets/posture_tree/posture_list_item.dart
@@ -1,6 +1,7 @@
 import 'package:acroulette/widgets/dialogs/posture_dialog/edit_posture_dialog.dart';
 import 'package:acroulette/widgets/icons/icons.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 class PostureListItem extends StatelessWidget {
   const PostureListItem(
@@ -27,58 +28,71 @@ class PostureListItem extends StatelessWidget {
   Widget build(BuildContext context) {
     ThemeData theme = Theme.of(context);
     return Card(
-        child: SizedBox(
-            height: 50,
-            child: Row(
-              children: [
-                Container(
-                  width: 5,
-                ),
-                postureIcon,
-                Container(
-                  width: 10,
-                ),
-                Center(child: Text(postureLabel)),
-                const Spacer(),
-                IconButton(
-                  constraints: const BoxConstraints(minWidth: 32),
-                  padding: const EdgeInsets.all(0),
-                  icon: const Icon(Icons.edit),
-                  tooltip: 'Edit position',
-                  onPressed: () {
-                    showDialog(
-                        context: context,
-                        builder: (BuildContext context) {
-                          return EditPosture(
-                              path: path,
-                              onEditClick: onEditClick,
-                              validator: (posture) {
-                                if (validator == null) return null;
-                                return validator!(true, posture);
-                              });
-                        }).then((exit) {
-                      if (exit) return;
-                    });
-                  },
-                ),
-                SizedBox(
-                    height: 24.0,
-                    width: 32.0,
-                    child: Switch(
-                      value: isSwitched,
-                      onChanged: enabled ? onChanged : null,
-                      activeColor: enabled
-                          ? theme.toggleButtonsTheme.color
-                          : theme.toggleButtonsTheme.disabledColor,
-                    )),
-                IconButton(
-                  constraints: const BoxConstraints(minWidth: 32),
-                  padding: const EdgeInsets.all(0),
-                  icon: const Icon(Icons.delete),
-                  tooltip: 'Delete position',
-                  onPressed: () => showDeletePositionDialog(context),
-                )
-              ],
-            )));
+      child: SizedBox(
+        height: 50,
+        child: Slidable(
+          key: Key(postureLabel),
+          startActionPane: const ActionPane(
+            motion: ScrollMotion(),
+            children: [],
+          ),
+          endActionPane: const ActionPane(
+            motion: ScrollMotion(),
+            children: [],
+          ),
+          child: Row(
+            children: [
+              Container(
+                width: 5,
+              ),
+              postureIcon,
+              Container(
+                width: 10,
+              ),
+              Center(child: Text(postureLabel)),
+              const Spacer(),
+              IconButton(
+                constraints: const BoxConstraints(minWidth: 32),
+                padding: const EdgeInsets.all(0),
+                icon: const Icon(Icons.edit),
+                tooltip: 'Edit position',
+                onPressed: () {
+                  showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return EditPosture(
+                            path: path,
+                            onEditClick: onEditClick,
+                            validator: (posture) {
+                              if (validator == null) return null;
+                              return validator!(true, posture);
+                            });
+                      }).then((exit) {
+                    if (exit) return;
+                  });
+                },
+              ),
+              SizedBox(
+                  height: 24.0,
+                  width: 32.0,
+                  child: Switch(
+                    value: isSwitched,
+                    onChanged: enabled ? onChanged : null,
+                    activeColor: enabled
+                        ? theme.toggleButtonsTheme.color
+                        : theme.toggleButtonsTheme.disabledColor,
+                  )),
+              IconButton(
+                constraints: const BoxConstraints(minWidth: 32),
+                padding: const EdgeInsets.all(0),
+                icon: const Icon(Icons.delete),
+                tooltip: 'Delete position',
+                onPressed: () => showDeletePositionDialog(context),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
