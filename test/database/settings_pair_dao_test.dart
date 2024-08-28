22228f8cfd61351adb4e39d563e7ec18a483e186
Alexander Sambale <Alexander.Sambale@samxela.eu>
2024-08-28 17:54:31 +0200

    Update CRUD test for settings_pair_dao

--- test/database/settings_pair_dao_test.dart
+++ test/database/settings_pair_dao_test.dart
@@ -1,3 +1,4 @@
+import 'package:acroulette/models/dao/settings_pair_dao.dart';
 import 'package:acroulette/models/database.dart';
 import 'package:acroulette/models/entities/settings_pair.dart';
 import 'package:acroulette/models/pair.dart';
@@ -6,9 +7,11 @@ import 'package:flutter_test/flutter_test.dart';
 void main() {
   group('settings_pair_dao', () {
     late AppDatabase database;
+    late SettingsPairDao settingsPairDao;
 
     setUp(() async {
       database = await $FloorAppDatabase.inMemoryDatabaseBuilder().build();
+      settingsPairDao = database.settingsPairDao;
     });
 
     tearDown(() async {
@@ -17,8 +20,8 @@ void main() {
 
     test('create SettingsPair', () async {
       SettingsPair settingsPair = SettingsPair(null, 'key', 'value');
-      await database.settingsPairDao.insertObject(settingsPair);
-      SettingsPair? output = await database.settingsPairDao.findEntityById(1);
+      await settingsPairDao.insertObject(settingsPair);
+      SettingsPair? output = await settingsPairDao.findEntityById(1);
       expect(output, isNot(null));
     });
 
@@ -26,9 +29,8 @@ void main() {
       String key = 'key';
       String value = 'value';
       SettingsPair settingsPair = SettingsPair(null, key, value);
-      await database.settingsPairDao.insertObject(settingsPair);
-      SettingsPair? output =
-          await database.settingsPairDao.findEntityByKey(key);
+      await settingsPairDao.insertObject(settingsPair);
+      SettingsPair? output = await settingsPairDao.findEntityByKey(key);
       expect(output!.value, value);
     });
 
@@ -37,12 +39,11 @@ void main() {
       String value = 'value';
       String expected = 'expected';
       SettingsPair? settingsPair = SettingsPair(null, key, value);
-      await database.settingsPairDao.insertObject(settingsPair);
-      settingsPair = await database.settingsPairDao.findEntityByKey(key);
+      await settingsPairDao.insertObject(settingsPair);
+      settingsPair = await settingsPairDao.findEntityByKey(key);
       settingsPair?.value = expected;
-      await database.settingsPairDao.updateObject(settingsPair!);
-      SettingsPair? output =
-          await database.settingsPairDao.findEntityByKey(key);
+      await settingsPairDao.updateObject(settingsPair!);
+      SettingsPair? output = await settingsPairDao.findEntityByKey(key);
       expect(output!.value, expected);
     });
 
@@ -57,14 +58,33 @@ void main() {
         Pair(key, value),
         Pair(key3, value),
       ];
-      await database.settingsPairDao.setDefaultValues(defaultValues);
+      await settingsPairDao.setDefaultValues(defaultValues);
       SettingsPair? defaultValue;
-      defaultValue = await database.settingsPairDao.findEntityByKey(key);
+      defaultValue = await settingsPairDao.findEntityByKey(key);
       expect(defaultValue, isNotNull);
-      defaultValue = await database.settingsPairDao.findEntityByKey(key1);
+      defaultValue = await settingsPairDao.findEntityByKey(key1);
       expect(key1, isNotNull);
-      defaultValue = await database.settingsPairDao.findEntityByKey(key3);
+      defaultValue = await settingsPairDao.findEntityByKey(key3);
       expect(key3, isNotNull);
     });
+
+    test(
+      "find all",
+      () async {
+        String key = 'key';
+        String key1 = '${key}1';
+        String key3 = '${key}3';
+        String value = 'value';
+        List<Pair> defaultValues = [
+          Pair(key1, value),
+          Pair(key, value),
+          Pair(key, value),
+          Pair(key3, value),
+        ];
+        await settingsPairDao.setDefaultValues(defaultValues);
+        List<SettingsPair> settings = await settingsPairDao.findAll();
+        expect(settings.length, defaultValues.length - 1);
+      },
+    );
   });
 }
